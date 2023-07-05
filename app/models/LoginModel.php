36d648da4ced6a9b444cf1b337487baac4cfc4e9
Irgo-25 <112465069+Irgo-25@users.noreply.github.com>
2023-07-05 23:50:23 +0700

    Update LoginModel.php

--- app/models/LoginModel.php
+++ app/models/LoginModel.php
@@ -10,7 +10,7 @@ class LoginModel
 
     public function getUser($username, $password)
     {
-        $this->db->query("SELECT * FROM user WHERE username = $username, password = $password");
+        $this->db->query("SELECT * FROM user WHERE username = :username, password = :password");
         $this->db->bind('username', $username);
         $this->db->bind('password', $password);
         return $this->db->resultSet();
